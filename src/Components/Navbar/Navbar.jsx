240fbaad0865052b1eefe0139fe0f00ff73b20e1
Md.limon islam <mdlimonislam134@gmail.com>
2025-10-07 21:58:45 +0600

    responsive navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,15 +14,10 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            <NavLink to="/">Home</NavLink>
+                            <NavLink to="/apps">Apps</NavLink>
+                            <NavLink>Installation</NavLink>
+
                         </ul>
                     </div>
                     <img className='w-[40px] h-[40px]' src={logoImg} alt="" />
@@ -33,7 +28,7 @@ const Navbar = () => {
                         <NavLink to="/">Home</NavLink>
                         <NavLink to="/apps">Apps</NavLink>
                         <NavLink>Installation</NavLink>
-              
+
                     </ul>
                 </div>
                 <div className="navbar-end">
